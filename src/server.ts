4ea22e2ccb744dc9447cf58fdfcf0b7adf1d2656
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-07 20:03:23 +0100

    Remove close method from Server class

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,10 @@
 import express, { Application } from 'express';
-import { Server as HttpServer } from 'http';
 import { Logger } from './common/utils/logger';
 import { ResourcesInitiator } from './common/utils/resources-initiator';
 
 export class Server {
     private readonly _port: number;
     private readonly _app: Application;
-    private _server: HttpServer | undefined;
 
     constructor(port: number) {
         this._port = port;
@@ -15,10 +13,6 @@ export class Server {
 
     public async start(): Promise<void> {
         await ResourcesInitiator.init(this._app);
-        this._server = this._app.listen(this._port, () => Logger.log(`Server is running on port ${this._port}`));
-    }
-
-    public stop(): void {
-        this._server?.close();
+        this._app.listen(this._port, () => Logger.log(`Server is running on port ${this._port}`));
     }
 }
