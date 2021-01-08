1122ad97bef842b89722455724efbaf422fb2a1d
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-08 13:44:35 +0100

    Refactor services/book-api/book-api.service.ts

--- src/services/book-api/book-api.service.ts
+++ src/services/book-api/book-api.service.ts
@@ -5,13 +5,22 @@ import { IBookApiService } from './interfaces/IBookApiService';
 
 export class BookApiService implements IBookApiService {
     public async getBooksData(query: string, maxResults: string = '3'): Promise<IBookData[]> {
-        const response = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&key=${config.BOOKS.API_KEY}`, {
-            headers: {
-                'Accept-Encoding': 'gzip',
-                'User-Agent': 'node-api (gzip)'
-            }
-        });
+        return this._requestBooksData(query, maxResults);
+    }
 
+    private async _requestBooksData(query: string, maxResults: string): Promise<IBookData[]> {
+        const response = await axios.get(this._buildUrl(query, maxResults), { headers: this._getCompressionHeaders() });
         return response.data.items;
+    }
+
+    private _buildUrl(query: string, maxResults: string): string {
+        return `https://www.googleapis.com/books/v1/volumes?q=${query}&maxResults=${maxResults}&key=${config.BOOKS.API_KEY}`
+    }
+
+    private _getCompressionHeaders(): Record<string, string> {
+        return {
+            'Accept-Encoding': 'gzip',
+            'User-Agent': 'node-api (gzip)'
+        }
     }
 }
