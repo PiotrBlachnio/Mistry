2ac33b564b64510127578115f779d73d2097ceeb
Piotr Blachnio <piotr.blachnio@yahoo.com>
2021-01-11 11:39:29 +0100

    Create services/omdb-api/omdb-api.service.ts

--- src/services/http/interfaces/IHttpService.ts
+++ src/services/http/interfaces/IHttpService.ts
@@ -1,5 +1,5 @@
 import { IHttpResponse } from './IHttpResponse';
 
 export interface IHttpService {
-    makeGetRequest(url: string, headers: Record<string, string>): Promise<IHttpResponse>; 
+    makeGetRequest(url: string, headers?: Record<string, string>): Promise<IHttpResponse>; 
 }
--- src/services/omdb-api/omdb-api.service.ts
+++ src/services/omdb-api/omdb-api.service.ts
@@ -1,5 +1,7 @@
+import { UrlBuilder } from '../../common/utils/url-builder';
 import { IGetManyBooksParameters } from '../google-api/interfaces/IGetManyBooksParameters';
 import { AxiosHttpService } from '../http/axios.service';
+import { IHttpResponse } from '../http/interfaces/IHttpResponse';
 import { IHttpService } from '../http/interfaces/IHttpService';
 import { IMovieData } from './interfaces/IMovieData';
 
@@ -7,6 +9,27 @@ export class OmdbApiService {
     constructor(private readonly _httpService: IHttpService = new AxiosHttpService()) {}
 
     public async getManyMovies(parameters: IGetManyBooksParameters): Promise<IMovieData[]> {
+        const response = await this._httpService.makeGetRequest(UrlBuilder.getManyMoviesUrl(parameters));
+        const movies = response.data.Search || [];
+
+        return movies;
+    }
+
+    public async getMovieById(id: string): Promise<IMovieData | null> {
+        const response = await this._httpService.makeGetRequest(UrlBuilder.getMovieByIdUrl(id));
         
+        if(response.data.Error) return null;
+
+        return this._mapResponseToMovieData(response);
+    }
+
+    public _mapResponseToMovieData(response: IHttpResponse): IMovieData {
+        return {
+            Title: response.data.Title,
+            Year: response.data.Year,
+            imdbID: response.data.imdbID,
+            Type: response.data.Type,
+            Poster: response.data.Poster
+        };
     }
 }
